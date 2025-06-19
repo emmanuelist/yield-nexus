6862ad7306cf056fc3213a63126b0691a3b06793
TheSoftNode <thesoftnode@gmail.com>
2025-06-19 14:01:53 +0100

    Add the first version of HowItWorksSection of the yield nexus landing page

--- yield-nexus-frontend/app/(main)/page.tsx
+++ yield-nexus-frontend/app/(main)/page.tsx
@@ -1,11 +1,13 @@
 import YieldNexusFeatures from "@/components/Landing/Features/YieldNexusFeatures";
 import YieldNexusHero from "@/components/Landing/Hero/YieldNexusHero";
+import YieldNexusHowItWorks from "@/components/Landing/HowItWorks/YieldNexusHowItWorks";
 
 export default function Home() {
   return (
     <>
       <YieldNexusHero />
       <YieldNexusFeatures />
+      <YieldNexusHowItWorks />
     </>
   );
 }
